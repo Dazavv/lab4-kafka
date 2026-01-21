375b0a97cec0fc6f12897cc4f9aae363261ae88a
dasha <dasha7348@gmail.com>
2026-01-21 11:11:07 +0300

    feat: change some settings

--- create-topics.sh
+++ create-topics.sh
@@ -3,6 +3,6 @@ echo "Waiting for Kafka..."
 sleep 30
 
 echo "Creating topics..."
-kafka-topics --bootstrap-server kafka-broker-1:29092 --create --if-not-exists --topic user-role-added --partitions 3 --replication-factor 3 --config min.insync.replicas=1
-kafka-topics --bootstrap-server kafka-broker-1:29092 --create --if-not-exists --topic user-events --partitions 3 --replication-factor 3 --config min.insync.replicas=2
+kafka-topics --bootstrap-server kafka-broker-1:29092 --create --if-not-exists --topic user-role-added-topic --partitions 3 --replication-factor 3 --config min.insync.replicas=1
+kafka-topics --bootstrap-server kafka-broker-1:29092 --create --if-not-exists --topic user-events --partitions 3 --replication-factor 3 --config min.insync.replicas=1
 kafka-topics --bootstrap-server kafka-broker-1:29092 --list
